9f2c0316d5e9d2efc4b03851ede14240979e3619
ZaZZiX <81529822+Zazzix@users.noreply.github.com>
2023-05-03 14:18:59 +0200

    implement a round

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const choices = ['rock', 'paper', 'scissors'];
+const playerSelection = prompt('Choose Rock, Paper or Scissors.').toLowerCase();
 
 
+// Function to get the compurer choice.
 function getComputerChoice(choices) {
     const randomIndex = Math.floor(Math.random() * choices.length); 
 
@@ -9,4 +11,21 @@ function getComputerChoice(choices) {
     return item;
 }
 
-console.log(getComputerChoice(choices));
+const computerSelection = getComputerChoice(choices);
+
+
+// Function to play a single round.
+function playRound(playerSelection, computerSelection) {
+    switch(true) {
+        case computerSelection === 'rock' && playerSelection === 'scissors':
+        case computerSelection === 'paper' && playerSelection === 'rock':
+        case computerSelection === 'scissors' && playerSelection === 'paper':
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        case computerSelection === playerSelection:
+            return `It's a tie! ${computerSelection} can't beat ${playerSelection}`;
+        default:
+            return `You win! ${playerSelection} beats ${computerSelection}`
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
